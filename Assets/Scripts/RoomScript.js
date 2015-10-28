a36f4082ce8c8a027d474e2d2cb63155e3c73095
Jason Mathias <jason.mathias@gmail.com>
2015-10-27 21:53:03 -0700

    First commit of scene!

---
+++ Assets/Scripts/RoomScript.js
@@ -0,0 +1,38 @@
+﻿#pragma strict
+
+
+public var ball: GameObject;
+public var lights: GameObject;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter()
+{
+	
+}
+
+
+
+//ignore these functions. Will explain later
+
+
+
+function dropBall()
+{
+
+	var ball2: GameObject = Instantiate(ball);
+}
+
+
+function turnOffLights()
+{
+
+	lights.SetActive(false);
+
+}
